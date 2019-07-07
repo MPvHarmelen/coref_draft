2c71547e6eb26b27d99846ad26ee0cb25494f022
Martin van Harmelen <Martin@vanharmelen.com>
2019-07-07 12:02:49 +0200

    Add `Entity` definition. Not used anywhere yet!

---
+++ multisieve_coreference/entity.py
@@ -0,0 +1,96 @@
+class Entity:
+    """
+    A cluster of mentions that refer to the same concept.
+
+    No promises are made about the order or uniqueness of mentions.
+
+    Following multi-sieve coreference (Lee et al., 2013), an entity can be
+    queried for any attribute that a mention has, and will return the set of
+    their values.
+
+    If some of the mentions have an attribute that others don't, Entity will
+    quietly exclude the mentions that don't have the attribute and return the
+    set of values for the mentions that do have it.
+    """
+
+    def __init__(self, mentions):
+        self.mentions = list(mentions)
+        if not self.mentions:
+            raise ValueError("An Entity must consist of at least one Mention.")
+
+    def __repr__(self):
+        return self.__class__.__name__ + f"({self.mentions!r})"
+
+    def __eq__(self, other):
+        return isinstance(other, Entity) and self.mentions == other.mentions
+
+    def __iter__(self):
+        return iter(self.mentions)
+
+    def __contains__(self, mention):
+        return mention in self.mentions
+
+    def merge(self, entity):
+        """
+        Merge a given Entity into this one.
+
+        No attention is paid to whether any mention in `entity` is already in
+        this one. Consequently, this Entity may contain duplicates after
+        merging.
+        """
+        self.mentions.extend(entity)
+
+    def non_unique_mention_attr(self, attr):
+        """
+        Get a list of mention attribute values
+
+        Crudely: `[getattr(mention, attr) for mention in self]`
+
+        Raise AttributeError if none of the mentions contained in the entity
+        have the requested attribute.
+
+        If at least one mention has the requested attribute, quietly leave out
+        the mentions that don't.
+        """
+        # Try to get all answers
+        all_answers = [
+            getattr(m, attr) for m in self if hasattr(m, attr)
+        ]
+        if not all_answers:
+            raise AttributeError(
+                f"None of the mentions have the attribute {attr!r}")
+        return all_answers
+
+    def mention_attr(self, attr):
+        """
+        Get the set of the values returned by `self.non_unique_mention_attr`
+
+        Crudely: `{getattr(mention, attr) for mention in self}`
+
+        Raise AttributeError if none of the mentions contained in the entity
+        have the requested attribute.
+
+        If at least one mention has the requested attribute, quietly leave out
+        the mentions that don't.
+        """
+        return set(self.non_unique_mention_attr(attr))
+
+    def flat_mention_attr(self, attr):
+        """
+        Get the union of the values returned by `self.non_unique_mention_attr`
+
+        Crudely: `{elem for itr in self.mention_attr(att) for elem in itr}`
+
+        Useful if the value is an iterable for every mention
+
+        Raise AttributeError if none of the mentions contained in the entity
+        have the requested attribute.
+
+        If at least one mention has the requested attribute, quietly leave out
+        the mentions that don't.
+        """
+        return {
+            elem
+            for itr in self.non_unique_mention_attr(attr)
+            for elem in itr
+        }
---
+++ tests/test_entity.py
@@ -0,0 +1,127 @@
+from copy import copy
+
+import pytest
+
+from multisieve_coreference.entity import Entity
+from multisieve_coreference.mentions import Mention
+
+
+HASHABLE_AND_ITERABLE_MENTION_ATTRIBUTES = [
+        'span',
+        'relaxed_span',
+        'full_head',
+        'modifiers',
+        'appositives',
+        'predicatives',
+        'non_stopwords',
+        'main_modifiers',
+    ]
+
+
+SINGLETON_MENTION_ATTRIBUTES = [
+        'head_offset',
+        'begin_offset',
+        'end_offset',
+        'head_pos',
+        'number',
+        'gender',
+        'person',
+        'entity_type',
+        'in_quotation',
+        'is_relative_pronoun',
+        'is_reflexive_pronoun',
+        'sentence_number',
+    ]
+
+
+ITERABLE_MENTION_ATTRIBUTES = ['coreference_prohibited'] + \
+    HASHABLE_AND_ITERABLE_MENTION_ATTRIBUTES
+
+
+HASHABLE_MENTION_ATTRIBUTES = HASHABLE_AND_ITERABLE_MENTION_ATTRIBUTES + \
+    SINGLETON_MENTION_ATTRIBUTES
+
+
+ALL_MENTION_ATTRIBUTES = ITERABLE_MENTION_ATTRIBUTES + \
+    SINGLETON_MENTION_ATTRIBUTES
+
+
+@pytest.fixture
+def some_mentions():
+    return [
+        Mention(id=1, span=[1, 2, 5], person='random'),
+        Mention(id=3, span=[17, 23, 50], person='random'),
+        Mention(id=2, span=[15, 16, 18], person='different'),
+    ]
+
+
+@pytest.fixture
+def some_other_mentions():
+    return [
+        Mention(id=6, span=[7, 14, 16], person='another random'),
+        Mention(id=8, span=[5, 8, 9], person='different'),
+        Mention(id=7, span=[10, 12, 17], person='random'),
+    ]
+
+
+@pytest.fixture
+def an_entity(some_mentions):
+    return Entity(some_mentions)
+
+
+@pytest.fixture
+def another_entity(some_other_mentions):
+    return Entity(some_other_mentions)
+
+
+@pytest.mark.parametrize('attr', ALL_MENTION_ATTRIBUTES)
+def test_non_unique_mention_attr(entity, attr):
+    assert len(entity.non_unique_mention_attr(attr)) == \
+        len(entity.mentions)
+
+
+@pytest.mark.parametrize('attr', HASHABLE_MENTION_ATTRIBUTES)
+def test_mention_attr(entity, attr):
+    unique = entity.mention_attr(attr)
+    full = entity.non_unique_mention_attr(attr)
+    assert 0 < len(unique) <= len(full)
+    assert set(full) == set(unique)
+
+
+@pytest.mark.parametrize('attr', ITERABLE_MENTION_ATTRIBUTES)
+def test_flat_mention_attr(entity, attr):
+    flat = entity.flat_mention_attr(attr)
+    assert {
+        elem
+        for iterable in entity.non_unique_mention_attr(attr)
+        for elem in iterable
+    } == flat
+
+
+@pytest.mark.parametrize('method', [
+    Entity.non_unique_mention_attr,
+    Entity.mention_attr,
+    Entity.flat_mention_attr,
+])
+def test_non_unique_mention_attr_fails(entity, method):
+    with pytest.raises(AttributeError):
+        method(entity, 'blablablfoodoesnotexist')
+
+
+@pytest.mark.xfail(reason="Equality of Entity is defined by equality of"
+                   " Mention, which is badly defined.", error=AssertionError)
+def test_repr(entity):
+    assert entity == eval(repr(entity))
+
+
+def test_merge(an_entity, another_entity):
+    merged = copy(an_entity)
+    merged.merge(another_entity)
+    assert all(entity in merged for entity in an_entity)
+    assert all(entity in merged for entity in another_entity)
+
+
+@pytest.fixture(params=[
+    pytest.lazy_fixture('an_entity'), pytest.lazy_fixture('another_entity')])
+def entity(request):
+    return request.param
