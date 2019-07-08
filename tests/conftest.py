564a782028cde0ddf485dd811d0a20a0f41130df
Martin van Harmelen <Martin@vanharmelen.com>
2019-07-08 16:38:38 +0200

    Set a memory limit in conftest.py to assure MemoryErrors don't crash the system

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,39 @@
 import os
 import pytest
+import resource
+
+
+def set_memory_limit(limit=2 * 1024**3):
+    """
+    To make sure tests that cause memory overflow do not crash the system,
+    the soft limit of memory is set quite low.
+
+    Luckily, this also seems to set the limit for child processes.
+
+    The default is 2 GiB.
+
+    :param limit:    new memory limit in bytes
+    """
+    memory_resource = resource.RLIMIT_VMEM \
+        if hasattr(resource, 'RLIMIT_VMEM') \
+        else resource.RLIMIT_AS
+
+    original_soft, original_hard = resource.getrlimit(memory_resource)
+
+    if original_hard == resource.RLIM_INFINITY or limit <= original_hard:
+        print(
+            f"Set memory limit to {limit/1024**2:3g} MiB")
+        resource.setrlimit(memory_resource, (limit, original_hard))
+    else:
+        print(
+            "Could not change original soft limit of"
+            f" {original_soft/1024**3:g} GiB"
+            f" to {limit/1024**3:g} GiB."
+            f" Original hard limit: {original_hard/1024**3:g} GiB"
+        )
+
+
+set_memory_limit()
 
 pytest.register_assert_rewrite('run_and_compare')
 
